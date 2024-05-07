4adabdd7cdfe1a4704e7e6a8ad0bc66ec37529d0
JD John <ubunteen2@gmail.com>
2024-05-07 08:25:55 -0400

    Finished using clerk authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+    return (
+      <div className="p-5">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    );
+  }
+
+  export default SetupPage;
+  
